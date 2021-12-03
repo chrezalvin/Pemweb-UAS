de7393338b2bdc98d1f14d627d645e358d0d4f15
chrezalvin <chrezalvin@gmail.com>
2021-12-03 08:42:20 +0700

    Adding comments
    adding comments to help

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -1,6 +1,7 @@
 <?php
     defined('BASEPATH') OR exit('No direct script access allowed');
 
+    // Note: Login, Register, and logout are stored in different classes.
     class Login extends CI_Controller {
 
         public function __construct() {
